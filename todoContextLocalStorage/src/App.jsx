ca4197b4dd391004b9522e6eaa4268dd5b7a46d8
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-06-27 15:43:30 +0500

    some changes

--- todoContextLocalStorage/src/App.jsx
+++ todoContextLocalStorage/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
+    setTodos((prev) => prev.map((prevTodo) => prevTodo.id === id ? {...prevTodo, completed: !prevTodo.completed} : prevTodo))
   }
 
   useEffect(() => {
