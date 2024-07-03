4a5c9d0480caf8fff454af1a7873eaa0ba724b7f
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-07-03 15:08:28 +0500

    reactRedux project created

---
+++ reactRedux/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <button className='button' aria-label='increment value' onClick={() => {}}>+</button>
+      <span className='value'>0</span>
+      <button className='button' aria-label='decrement value' onClick={() => {}}>-</button>
+    </div>
+  )
+}
+
+export default App
