eeeb95366b30bef53cb6fab3364466f6fa5233ee
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-07-10 20:56:49 +0500

    created reducer

--- reactRedux/src/features/counter/Counter.jsx
+++ reactRedux/src/features/counter/Counter.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './counterSlice'
+
 function Counter() {
-  const [counter, setCounter] = useState(0)
+  const count = useSelector((state) => state.counter.count)
+  const dispatch = useDispatch()
   return (
     <div>
-      <button className='button' aria-label='increment value' onClick={() => {setCounter(counter+1)}}>+</button>
-      <span className='value'>counter: {counter}</span>
-      <button className='button' aria-label='decrement value' onClick={() => {setCounter(counter-1)}}>-</button>
+      <button className='button' aria-label='increment value' onClick={() => { dispatch(increment()) }}>+</button>
+      <span className='value'>counter: {count}</span>
+      <button className='button' aria-label='decrement value' onClick={() => { dispatch(decrement()) }}>-</button>
     </div>
   )
 }
