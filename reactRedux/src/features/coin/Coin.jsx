992d92ddecbfa6e51d0db7fd1cea83d71648e439
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-07-15 16:49:34 +0500

    completed the redux implementation

--- reactRedux/src/features/coin/Coin.jsx
+++ reactRedux/src/features/coin/Coin.jsx
@@ -1,8 +1,9 @@
-
+import { useSelector } from "react-redux"
 function Coin() {
+  const count = useSelector((state) => state.counter.count)
   return (
     <div>
-      <span className='value'>coin: {counter}</span>
+      <span className='value'>coin: {count}</span>
     </div>
   )
 }
--- reactRedux/src/features/counter/Counter.jsx
+++ reactRedux/src/features/counter/Counter.jsx
@@ -1,5 +1,5 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { increment, decrement } from './counterSlice'
+import { increment, decrement, incrementByAmount } from './counterSlice'
 
 function Counter() {
   const count = useSelector((state) => state.counter.count)
@@ -9,6 +9,7 @@ function Counter() {
       <button className='button' aria-label='increment value' onClick={() => { dispatch(increment()) }}>+</button>
       <span className='value'>counter: {count}</span>
       <button className='button' aria-label='decrement value' onClick={() => { dispatch(decrement()) }}>-</button>
+      <button className='button' aria-label='' onClick={() => { dispatch(incrementByAmount(10)) }}>increment by 10</button>
     </div>
   )
 }
