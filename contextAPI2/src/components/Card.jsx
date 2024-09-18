c338b41107c921cefbb69112d88cf03ca5f5577f
MuhammadQasimFiaz <qmughal794@gmail.com>
2024-09-18 17:38:36 +0500

    removed extra tags

--- contextAPI2/src/components/Card.jsx
+++ contextAPI2/src/components/Card.jsx
@@ -2,15 +2,11 @@ import Image from '../assets/pexels.jpg'
 export default function Card() {
   return (
       <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-          <a href="/">
               <img className="p-8 rounded-t-lg" src={Image} alt="product_image1" />
-          </a>
           <div className="px-5 pb-5">
-              <a href="/">
                   <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                      Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
+                      Italian Food
                   </h5>
-              </a>
               <div className="flex items-center mt-2.5 mb-5">
                   <svg
                       className="w-4 h-4 text-yellow-300 mr-1"
