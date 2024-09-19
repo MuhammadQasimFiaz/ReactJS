8447e49cc4ad3946eea00197b0a93a6e4a72fc69
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-09-19 11:56:04 +0500

    some changes

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 
 function App() {
-  const [length, setLength] = useState(8)  
+  const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false)
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
@@ -9,7 +9,7 @@ function App() {
   const passwordRef = useRef(null)
 
 
-  const passwordGenerator = useCallback( () => {
+  const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
@@ -22,7 +22,7 @@ function App() {
 
     setPassword(pass)
 
-  } , [length, numberAllowed, charAllowed, setPassword])
+  }, [length, numberAllowed, charAllowed, setPassword])
 
   const copyPasswordtoClipBoard = useCallback(() => {
     passwordRef.current?.select()
@@ -39,13 +39,13 @@ function App() {
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
         <h1 className='text-white text-center my-3'>Password Generator</h1>
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
-          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly ref={passwordRef}/>
+          <input type="text" value={password} className='outline-none w-full py-1 px-3' placeholder='password' readOnly ref={passwordRef} />
           <button onClick={copyPasswordtoClipBoard} className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
         </div>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input type="range" min={6} max={50} value={length} className='cursor-pointer' id='length'
-              onChange={(e) => {setLength(e.target.value)}}
+              onChange={(e) => { setLength(e.target.value) }}
             />
             <label htmlFor="length">Length: {length}</label>
           </div>
