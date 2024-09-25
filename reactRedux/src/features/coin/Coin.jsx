d44656d02e8df0c729492fdf5eca3f91e4aaa0f0
MuhammadQasimFiaz <80813627+MuhammadQasimFiaz@users.noreply.github.com>
2024-09-25 13:08:16 +0500

    some changes

--- reactRedux/src/features/coin/Coin.jsx
+++ reactRedux/src/features/coin/Coin.jsx
@@ -3,7 +3,7 @@ function Coin() {
   const count = useSelector((state) => state.counter.count)
   const themeTextColor = useSelector((state) => state.theme.color)
   return (
-    <div className="flex justify-center bg-gray-300 m-auto p-2 rounded-lg w-fit">
+    <div className="flex justify-center bg-black m-auto p-2 rounded-lg w-fit">
       <span className='value' style={{color: themeTextColor}}>coin: {count}</span>
     </div>
   )
